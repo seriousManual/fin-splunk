c5e1abc6ede7bb57308ef203a3b2e6ef0a42827d
zaphod1984 <zaphod84@gmx.de>
2015-12-05 22:58:38 +0100

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+var config = require('./lib/config');
+var service = require('./lib/service');
+
+var reader = require('./lib/reader');
+var PositionStream = require('./lib/position/PositionStream');
+var Classification = require('./lib/classification/Classification');
+var ClassificationStream = require('./lib/classification/ClassificationStream');
+var SaveStream = require('./lib/save/SaveStream');
+
+var classification = new Classification(config.classification);
+var classificationStream = new ClassificationStream(classification);
+
+service.connect(function(error, service) {
+    if (error) {
+        throw error;
+    }
+
+    reader
+        .read(__dirname + '/data.csv')
+        .pipe(new PositionStream())
+        .pipe(classificationStream)
+        .pipe(new SaveStream(service))
+        .on('end', () => console.log('done'))
+        .on('error', (error) => console.log(error));
+});
