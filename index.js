9707f87d2241c18681fac8b8658bdf0d23314a79
seriousManual <seriousstuff@gmx.de>
2015-12-08 09:11:22 +0100

    integrate filters

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var service = require('./lib/service');
 
 var reader = require('./lib/reader');
 var PositionStream = require('./lib/position/PositionStream');
+var FilterStream = require('./lib/filter/FilterStream');
+var FilterData = require('./lib/filter/FilterData');
 var Classification = require('./lib/classification/Classification');
 var ClassificationStream = require('./lib/classification/ClassificationStream');
 var SaveStream = require('./lib/save/SaveStream');
@@ -15,6 +17,7 @@ service.connect(function(error, service) {
     reader
         .read(__dirname + '/data.csv')
         .pipe(new PositionStream())
+        .pipe(new FilterStream(new FilterData(service)))
         .pipe(new ClassificationStream(new Classification(config.classification)))
         .pipe(new SaveStream(service));
 });
