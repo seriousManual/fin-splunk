0022de73287473606bba99e856ed2fab0a6fa4be
seriousManual <seriousstuff@gmx.de>
2015-12-09 11:17:42 +0100

    add test for filterstream

--- tests/tests.js
+++ tests/tests.js
@@ -3,10 +3,19 @@ var expect = require('chai').expect
 
 var Position = require('../lib/position/Position')
 var FilterData = require('../lib/filter/FilterData')
+var FilterStream = require('../lib/filter/FilterStream')
 
 var createServiceMock = (error, result) => {
     return {
         oneshotSearch: sinon.spy((search, parameters, callback) => callback(error || null, result || null))
+    }
+}
+var createFilterDataMock = (error, contains) => {
+    var i = 0
+    contains = contains || []
+
+    return {
+        contains: sinon.spy((position, callback) => callback(error, contains[i++]))
     }
 }
 
@@ -66,6 +75,45 @@ describe('fin-splunk', () => {
                 it('should ask the service', () => expect(dummyService.oneshotSearch.args[0][1]).to.deep.equal({earliest_time: '-3mon', latest_time: 'now'}))
                 it('should be initialized', () => expect(filterData._isInitialized()).to.be.true)
                 it('should contain values', () => expect(result).to.be.false)
+            })
+        })
+
+        describe('stream', () => {
+            describe('error', () => {
+                var error;
+                var filterDataMock = createFilterDataMock(new Error('fooError'))
+                var filterStream = new FilterStream(filterDataMock)
+                
+                before((done) => {
+                    filterStream
+                        .on('error', (_error) => {
+                            error = _error
+                            done()
+                        })
+                        .write('foo')
+
+                    filterStream.write('bar')
+                    filterStream.write('bax')
+                })
+                
+                it('should return an error', () => expect(error.message).to.equal('fooError'))
+                it('ask the filterdata with correct value', () => expect(filterDataMock.contains.args[0][0]).to.equal('foo'))
+            })
+
+            describe('success', () => {
+                var filterDataMock = createFilterDataMock(null, [false, true, false])
+                var filterStream = new FilterStream(filterDataMock)
+                var collected = [];
+
+                before(() => {
+                    filterStream.write('foo')
+                    filterStream.write('bar')
+                    filterStream.write('bax')
+
+                    filterStream.on('data', (data) => collected.push(data))
+                })
+
+                it('should only let the correct values pass', () => expect(collected).to.deep.equal(['bar']))
             })
         })
     })
